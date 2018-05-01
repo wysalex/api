d89ef961c7f3a762e8fb6062a23caa1f6d0ceb8f
Alex Wang <wysalexwang@gmail.com>
2018-05-01 18:57:01 +0800

    新增 API https

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ * Api for Alex
+ * Alex Wang <wysalexwang@gmail.com>
+ */
+'use strict'
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
 const koa = require('koa')
 const route = require('koa-route')
 const cors = require('@koa/cors')
@@ -6,9 +14,23 @@ const app = new koa()
 app.use(cors())
 
 app.use(route.get('/', function () {
-  this.body = { response: 'test' }
+  this.body = `/**
+ * Api for Alex
+ * Alex Wang <wysalexwang@gmail.com>
+ */`
 }))
 
-app.listen(6666)
+app.use(route.get('/version', function () {
+  this.body = { version: '0.0.1' }
+}))
+
+// listen http/https
+const options = {
+  key: fs.readFileSync('/etc/nginx/ssl/nginx.key', 'utf8'),
+  cert: fs.readFileSync('/etc/nginx/ssl/nginx.crt', 'utf8')
+}
+// http.createServer(app.callback()).listen(8888)
+https.createServer(options, app.callback()).listen(8888)
+// app.listen(6666)
 
 console.log('Api service is on...')
